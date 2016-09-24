c284bbb9da1ce6d474be34bb8bccc18b1f7b0089
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 10:53:22 +0700

    try to change props but it not work

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ class App extends React.Component { // alternative way => class App extends Comp
   //   // this.state = {name: 'golfapipol.me'}
   // }
   render() {
-    // this.props.age = 10;
+    this.props.age = 10;
     return (<h1>Hello, React Component build by {this.props.name} {this.props.age + 1}</h1>);
   }
 }
