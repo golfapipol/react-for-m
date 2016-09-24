e8593c87379ea15c1cdaeb2d32d579c2d56914b1
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 10:43:25 +0700

    add how to access attribute

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component { // alternative way => class App extends Component {
   render() {
-    return (<h1>Hello, React Component</h1>);
+    return (<h1>Hello, React Component build by {this.props.name} {this.props.age + 1}</h1>);
   }
 }
 
 // ReactDOM.render(Component, targetElement)
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App name="golfapipol.me" age={23}/>, document.getElementById('root'))
 
