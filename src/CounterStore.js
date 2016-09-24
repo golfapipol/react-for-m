4c04527cbffca30397a50a36e011b51e7461ebaf
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 14:30:18 +0700

    create CounterStore

---
+++ src/CounterStore.js
@@ -0,0 +1,11 @@
+import {observable} from 'mobx';
+export default class CounterStore {
+  @observable count = 0;
+  
+  increase() {
+    this.count = this.count + 1;
+  }
+  decrease() {
+    this.count = this.count - 1;
+  }
+}
