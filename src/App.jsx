cf245e846e94d4910a7711a6a1b8b7657776972c
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 10:52:57 +0700

    split file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component { // alternative way => class App extends Component {
+  // constructor() {
+  //   // this.state = {name: 'golfapipol.me'}
+  // }
+  render() {
+    // this.props.age = 10;
+    return (<h1>Hello, React Component build by {this.props.name} {this.props.age + 1}</h1>);
+  }
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react'; // alternative way => import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component { // alternative way => class App extends Component {
-  render() {
-    return (<h1>Hello, React Component build by {this.props.name} {this.props.age + 1}</h1>);
-  }
-}
-
+import App from './App'
 // ReactDOM.render(Component, targetElement)
 ReactDOM.render(<App name="golfapipol.me" age={23}/>, document.getElementById('root'))
 
