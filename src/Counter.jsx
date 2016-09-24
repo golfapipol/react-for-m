66884bf409bbef6c5533930688a36d71db4c4388
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 12:20:47 +0700

    implement increase function

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class Counter extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { count: 0 };
+  }
+  increase = () => {
+    let newState = this.state.count + 1;
+    this.setState({count: newState})
+  }
   render() {
     return (
       <div>
-        <button className="increase">+</button>
-        <span>0</span>
+        <button className="increase" onClick={this.increase}>+</button>
+        <span>{this.state.count}</span>
         <button className="decrease">-</button>
       </div>
     );
