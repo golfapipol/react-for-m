2de8017dadf91b8c6a9bf5e5e28f38fb5dd11d73
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 11:42:10 +0700

    test add function

--- src/Counter.test.jsx
+++ src/Counter.test.jsx
@@ -1,5 +1,13 @@
-describe('Counter', () => {
-  it('setup', () => {
-    expect(true).toBe(false);
+function add(x, y) {
+  return x + y;
+}
+describe('Add', () => {
+  it('should be 2 if 1 add 1', () => {
+    //arrange
+
+    //act
+    let result = add(1, 1);
+    //assert
+    expect(result).toBe(2);
   })
 })
