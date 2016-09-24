ec50fd1f3a36c90c4defe304d0214d55f1e27da1
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 11:54:06 +0700

    add test lib and write first test

--- src/Counter.test.jsx
+++ src/Counter.test.jsx
@@ -1,13 +1,11 @@
-function add(x, y) {
-  return x + y;
-}
-describe('Add', () => {
-  it('should be 2 if 1 add 1', () => {
-    //arrange
+import React from 'react'
+import {shallow} from 'enzyme';
+import Counter from './Counter'
 
-    //act
-    let result = add(1, 1);
-    //assert
-    expect(result).toBe(2);
+describe('Counter', () => {
+  it('renders button -', () => {
+    let wrapper = shallow(<Counter />);
+    let expectedButton = (<button>-</button>)
+    expect(wrapper.containsMatchingElement(expectedButton)).toEqual(true);
   })
 })
