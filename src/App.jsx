ae63199d73413afcd5f25b9ca11ce7ae2b28dd74
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 11:01:50 +0700

    add text field but it not working

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ class App extends React.Component { // alternative way => class App extends Comp
     this.state = {name: 'Golf'}
   }
   
+  onChangeText(e) {
+    this.state.name = e.target.value;
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, React Component build by {this.state.name} {this.props.age + 1}</h1>
+        <input type="text" onChange={this.onChangeText} />
       </div>
     );
   }
