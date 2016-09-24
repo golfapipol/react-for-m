66d928a9d894b884dc432c5289c16375164f4a0e
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 14:06:24 +0700

    try mobx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'; // alternative way => import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import { observable, autorun } from 'mobx';
 import App from './App'
 // ReactDOM.render(Component, targetElement)
 ReactDOM.render(<App name="golfapipol.me" age={23}/>, document.getElementById('root'))
 
+let state = observable({
+  name: ''
+})
+
+autorun(() => {
+  console.log(state.name);
+})
+
+state.name = 'Golf';
