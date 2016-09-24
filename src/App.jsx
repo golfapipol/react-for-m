51b3679b68eda07dbe39f1cec5eaa887b6c784c5
Apipol Sukgler <apipol@computerlogy.com>
2016-09-24 11:01:27 +0700

    hello state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 class App extends React.Component { // alternative way => class App extends Component {
-  // constructor() {
-  //   // this.state = {name: 'golfapipol.me'}
-  // }
+  constructor() {
+    super();
+    this.state = {name: 'Golf'}
+  }
+  
   render() {
-    this.props.age = 10;
-    return (<h1>Hello, React Component build by {this.props.name} {this.props.age + 1}</h1>);
+    return (
+      <div>
+        <h1>Hello, React Component build by {this.state.name} {this.props.age + 1}</h1>
+      </div>
+    );
   }
 }
 
